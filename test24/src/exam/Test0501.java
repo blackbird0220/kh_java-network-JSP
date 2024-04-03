f5e7b8ffc7e40adcfdaa8aa5e93ac8e062a034f8
blackbird0220 <kewchb@naver.com>
2024-04-03 18:36:40 +0900

    java class network & JSP

--- test24/src/exam/Test0501.java
+++ test24/src/exam/Test0501.java
@@ -1,19 +1,27 @@
 package exam;
 
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.sun.jdi.connect.spi.Connection;
+
 public class Test0501 {
 
 	public static void main(String[] args) {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");		//url 이 잘못되어 
 			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "1234");
-			String query = select emp_from
-			PreparedStatement st = conn.prepareStatement(query);
 			
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
+			PreparedStatement st = conn.prepareStatement(query);
+			ResultSet rs = st.executeUpdate();
+			while(rs) {
+				System.out.println(rs.getString("empId")+);
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+		// 
 	}
 
 }
